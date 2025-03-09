cfed058a440b8c35470d47c3f19806cbd20b1ef0
Paulo <paulohsantosrocha@gmail.com>
2025-03-09 18:53:07 -0300

    chore: setting up the project

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hey, Paulo. How are you? Are you ok?</Text>
+    </View>
+  );
+}
