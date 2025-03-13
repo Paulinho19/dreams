77a3aedc7db6d2940717c9d2e4fe6dc673d1b02b
Paulo <paulohsantosrocha@gmail.com>
2025-03-13 17:34:27 -0300

    chore: font and images setup

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,6 +10,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="font-bold text-3xl my-10 font-rubik">
+        Bem-vindo ao Dreams!
+      </Text>
       <Link href="/sign-in">Cadastre-se</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Perfil</Link>
